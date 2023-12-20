120818158d64a27f335efc71a053b34328803484
karpunin <karpunin@satel.org>
2023-12-20 16:56:34 +0300

    matrix generetion done

--- matrix_generation.cpp
+++ matrix_generation.cpp
@@ -1,3 +1,24 @@
-//
-// Created by karpunin on 20.12.2023.
-//
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    cout << "matrix generation started";
+    fstream matrix_1("C:\\Users\\karpunin\\CLionProjects\\parallel\\matrix_1.txt", fstream::in | fstream::out | fstream::trunc);
+    fstream matrix_2("C:\\Users\\karpunin\\CLionProjects\\parallel\\matrix_2.txt", fstream::in | fstream::out | fstream::trunc);
+
+    for (int i = 0; i < 1000; ++i) {
+        for (int j  = 0; j < 1000; ++j) {
+            matrix_1 << rand() << " ";
+            matrix_2 << rand() << " ";
+        }
+        matrix_1 << "\n";
+        matrix_2 << "\n";
+    }
+
+
+    matrix_1.close();
+    matrix_2.close();
+    return 0;
+}
