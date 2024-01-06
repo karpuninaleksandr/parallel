750f0ab13964f1ab741d826b9496f537331aff88
karpunin <karpunin@satel.org>
2024-01-06 19:39:20 +0300

    two labs done

--- mpi_task.cpp
+++ mpi_task.cpp
@@ -1,3 +1,77 @@
-//
-// Created by karpunin on 20.12.2023.
-//
+#include <iostream>
+#include <fstream>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    ifstream matrix_1, matrix_2;
+
+    int rank, size, extraData, dataForProcess, n = 1000;
+
+    int* matrixA = new int[n * n], * matrixB = new int[n * n], * matrixResult = new int[n * n];
+
+    for (int i = 0; i < n * n; ++i) matrixA[i] = matrixB[i] = matrixResult[i] = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Status status;
+
+    dataForProcess = n / size;
+    extraData = (rank == size - 1 && n % size != 0) ? n % size : 0;
+
+    if (rank == 0) {
+        matrix_1.open("matrix_1.txt");
+        matrix_2.open("matrix_2.txt");
+
+        cout << "matrix reading started...\n";
+
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j) {
+                matrix_1 >> matrixA[i * n + j];
+                matrix_2 >> matrixB[i * n + j];
+            }
+
+        matrix_1.close();
+        matrix_2.close();
+
+        cout << "matrix reading done.\n";
+    }
+
+    cout << "matrix multiplication started...\n";
+
+    double startTime = MPI_Wtime();
+
+    for (int i = 1; i < size; ++i) {
+        MPI_Send(&matrixA[dataForProcess * i * n], (i == size - 1) ? (dataForProcess + n % size) * n : dataForProcess * n,
+                 MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+        MPI_Send(&matrixB[0], n * n, MPI_DOUBLE, i, 2, MPI_COMM_WORLD);
+    } else {
+        MPI_Recv(&A[dataForProcess * rank * n], (dataForProcess + extraData) * n, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&B[0], n * n, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, &status);
+    }
+
+    for (int i = dataForProcess * rank; i < dataForProcess * (rank + 1) + extraData; ++i)
+        for (int j = 0; j < n; ++j) {
+            matrixResult[i * n + j] = 0;
+            for (int t  = 0; t < n; ++t) matrixResult[i * n + j] += matrixA[i * n + t] * matrixB[t * n + j];
+        }
+
+    delete[] matrixA;
+    delete[] matrixB;
+
+    if (rank != 0) MPI_Send(&matrixResult[dataForProcess * rank * n], (dataForProcess + extraData) * n, MPI_DOUBLE, 0, 4, MPI_COMM_WORLD);
+    else {
+        for (int i = 1; i < size; ++i) MPI_Recv(&matrixResult[dataForProcess * i * n], (i == size - 1) ? (dataForProcess + n % size) * n :
+        dataForProcess * n, MPI_DOUBLE, i, 4, MPI_COMM_WORLD, &status);
+
+        double endTime = MPI_Wtime();
+
+        cout << "matrix multiplication ended.\ntime spent: " <<  endTime - startTime;
+    }
+
+    MPI_Finalize();
+    delete[] matrixResult;
+    return 0;
+}
---
+++ omp_task.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <omp.h>
+
+using namespace std;
+
+int main() {
+    ifstream matrix_1, matrix_2;
+    int amountOfThreads, a = 0, b = 0, c, n = 1000;
+
+    int** matrixA, ** matrixB, ** matrixResult;
+
+    matrixA = new int*[n];
+    matrixB = new int*[n];
+    matrixResult = new int*[n];
+
+    for (int i = 0; i < n; ++i) {
+        matrixA[i] = new int[n];
+        matrixB[i] = new int[n];
+        matrixResult[i] = new int[n];
+    }
+
+    matrix_1.open("matrix_1.txt");
+    matrix_2.open("matrix_2.txt");
+
+    cout << "matrix reading started...\n";
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j) {
+            matrix_1 >> matrixA[i][j];
+            matrix_2 >> matrixB[i][j];
+            matrixResult[i][j] = 0;
+        }
+
+    matrix_1.close();
+    matrix_2.close();
+
+    cout << "matrix reading done.\n" << "enter amount of threads: ";
+    cin >> amountOfThreads;
+
+    omp_set_num_threads(amountOfThreads);
+
+    cout << "matrix multiplication started...\n";
+
+    double startTime = omp_get_wtime();
+
+#pragma omp parallel num_threads(amountOfThreads)
+    {
+#pragma omp for private(a, b, c)
+            for (; a < n; ++a)
+                for (; b < n; ++b)
+                    for (c = 0; c < n; ++c) matrixResult[a][b] += matrixA[a][c] * matrixB[c][b];
+    }
+
+    double endTime = omp_get_wtime();
+
+    cout << "matrix multiplication ended.\namount of threads: " << amountOfThreads <<  "\ntime spent: " << endTime - startTime;
+
+    system("pause");
+    return 0;
+}
